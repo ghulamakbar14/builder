7db2c35c091a7d175cebe6728189bd2001163eb6
ghulamakbar14 <ghulamakbar14@gmail.com>
2019-05-07 01:27:03 -0700

    post data to php file

--- server.php
+++ server.php
@@ -1,7 +1,20 @@
 <?php
-//$type = $_POST['type'];
-//$data = $_POST['data'];
-//echo '<pre>';
-print_r($_POST);
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+//print_r($request);
+$type = $request->type;
+$data = $request->data;
 
+/* Config array posted. Save data array in concerned DB table */
+if($type == 'config'){
+    print_r($data);
+}
+/* Question array posted. Save data array in concerned DB table */
+if($type == 'question'){
+    print_r($data);
+}
+/* Data array posted. Save data array in concerned DB table */
+if($type == 'data'){
+    print_r($data);
+}
 ?>
